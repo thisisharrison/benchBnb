f9a6066288024b250e3f54e551d4da60f1fd15e5
thisisharrison <harrison.lau@outlook.com>
2021-01-09 16:32:45 +0800

    user model controller and session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user.nil?
+            render json: ['Wrong credentials'], status: 401
+        else
+            login!(@user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        if !current_user
+            render json: ["No Current User"], status: 404
+        end
+        logout!
+        render json: {}
+    end
+
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    # sets the app's session token to be user's
+
+    helper_method :current_user
+
+    def login!(user)
+        session[:session_token] = user.session_token
+    end
+
+    def logout!
+        # reset session token of the app
+        session[:session_token] = nil
+        current_user.reset_session_token!
+    end
+
+    # We will need this for preload state as well
+    def current_user
+        # check for app's session
+        return nil unless session[:session_token]
+        # return or find user with matching session token
+        @current_user ||= User.find(session_token: session[:session_token])
+    end
+
+    # rails would have 
+    # logged_in?
+    # require_logged_out
+    # require_logged_in
+    # BUT we will use Auth and Protected in frontend to handle this
+
 end
